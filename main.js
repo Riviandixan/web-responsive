8bfccecc6aedb51908cb54d99db07a0cb402a377
Riviandixan <riviandixanpurnadi@smkwikrama.sch.id>
2022-04-10 13:06:42 +0700

    website responsive

---
+++ main.js
@@ -0,0 +1,23 @@
+const menuIcon = documen.getElementById("menu-icon");
+const slideoutMenu = document.getElementById("slideout-menu");
+const searchIcon = document.getElementById("search-icon");
+
+searchIcon.addEventListener('click', function(){
+    if(searchBox.style.top == '72px'){
+        searchBox.style.top = '24px';
+        searchBox.style.pointerEvents = 'none';
+    }else{
+        searchBox.style.top = '72px';
+        searchBox.style.pointerEvents = 'auto';
+    }
+});
+
+menuIcon.addEventListener('click', function(){
+    if(slideoutMenu.style.opacity == "1"){
+        slideoutMenu.style.opacity = '0';
+        slideoutMenu.style.pointerEvents = 'none';
+    }else{
+        slideoutMenu.style.opacity = '1';
+        slideoutMenu.style.pointerEvents = 'auto';
+    }
+});
